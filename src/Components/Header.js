7687695fc2a39158f4011fa17a5422a8888f1465
Fenomen-Alex <alexfenomen@gmail.com>
2021-04-27 01:08:52 +0300

    upd router6

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,9 +5,10 @@ import {
   Form,
   FormControl,
   Nav,
-  Navbar} from 'react-bootstrap';
+  Navbar
+} from 'react-bootstrap';
 import logo from '../assets/star_wars_logo.png';
-import {HashRouter as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Home from '../Pages/Home';
 import About from '../Pages/About';
 import Contacts from '../Pages/Contacts';
@@ -17,43 +18,44 @@ class Header extends Component {
   render() {
     return (
       <>
-        <Navbar sticky="top" collapseOnSelect expand="md" bg="dark" variant="dark">
-          <Container>
-            <Navbar.Brand href="/">
-              <img
-                src={logo}
-                alt="Logo"
-                height="30"
-                width="30"
-                className="d-inline-block align-top"
-              /> My Blog
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="mr-auto">
-                <Nav.Link href="/Blog_Copy/">Home</Nav.Link>
-                <Nav.Link href="/Blog_Copy/about">About</Nav.Link>
-                <Nav.Link href="/Blog_Copy/contacts">Contacts</Nav.Link>
-                <Nav.Link href="/Blog_Copy/blog">Blog</Nav.Link>
-              </Nav>
-              <Form inline>
-                <FormControl
-                  type="text"
-                  placeholder="Search"
-                  className="mr-sm-2"
-                />
-                <Button variant="outline-info">Search</Button>
-              </Form>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-
         <Router basename={process.env.PUBLIC_URL}>
+          <Navbar sticky="top" collapseOnSelect expand="md" bg="dark" variant="dark">
+            <Container>
+              <Navbar.Brand href="/">
+                <img
+                  src={logo}
+                  alt="Logo"
+                  height="30"
+                  width="30"
+                  className="d-inline-block align-top"
+                /> My Blog
+              </Navbar.Brand>
+              <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+              <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                  <Nav.Link href="/">Home</Nav.Link>
+                  <Nav.Link href="/about">About</Nav.Link>
+                  <Nav.Link href="/contacts">Contacts</Nav.Link>
+                  <Nav.Link href="/blog">Blog</Nav.Link>
+                </Nav>
+                <Form inline>
+                  <FormControl
+                    type="text"
+                    placeholder="Search"
+                    className="mr-sm-2"
+                  />
+                  <Button variant="outline-info">Search</Button>
+                </Form>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+
+
           <Switch>
-            <Route exact path="/Blog_Copy/" component={Home} />
-            <Route exact path="/Blog_Copy/about" component={About} />
-            <Route exact path="/Blog_Copy/contacts" component={Contacts} />
-            <Route exact path="/Blog_Copy/blog" component={Blog} />
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/about" component={About}/>
+            <Route exact path="/contacts" component={Contacts}/>
+            <Route exact path="/blog" component={Blog}/>
           </Switch>
         </Router>
       </>
