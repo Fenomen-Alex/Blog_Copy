48162ea1881ca9be63008dd8dc89ae0f704f3877
Fenomen-Alex <alexfenomen@gmail.com>
2021-04-27 02:28:39 +0300

    edit name

--- src/Pages/Contacts.js
+++ src/Pages/Contacts.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import {Container} from "react-bootstrap";
 
 const Contacts = () => {
   return (
-    <div>
+    <Container>
       <iframe
         title="Form"
         src="https://docs.google.com/forms/d/e/1FAIpQLScYkdqVY-BN4RRf1SKFXirLRYnkt6xjVNgaVe8RIPmVou87Ww/viewform?embedded=true"
         width="640" height="852" frameBorder="0" marginHeight="0" marginWidth="0">Загрузка…
       </iframe>
-    </div>
+    </Container>
   );
 };
 
